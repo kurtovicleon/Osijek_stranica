cb201c11f9e9162629ef51e5bda9a8265ca36d40
kurtovicleon <kurtovicleon@gmail.com>
2018-01-09 00:37:59 +0200

    initialcommit

---
+++ login.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+
+<html class="no-js" lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Osijek log in</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/loginstil.css">
+    <style>
+     body
+     {background:url(osijek1.png) no-repeat center center fixed;
+      -webkit-backgroun-size:cover;
+      -moz-background-size:cover;
+      -o-background-size:cover;
+      background-size:cover;
+    }
+     </style>
+</head>
+	<body>
+
+    <div class="loginBox">
+			<img src="user.png" class="user">
+			<h2>Prijava</h2>
+			<form>
+				<p>Email</p>
+				<input type="text" name="" placeholder="Unesite Email">
+				<p>Lozinka</p>
+				<input type="password" name="" placeholder="Vaša lozinka">
+				<input type="submit" name="" value="Prijava">
+				<a href="#">Zaboravljena lozinka?</a>
+			</form>
+		</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	
+	
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
