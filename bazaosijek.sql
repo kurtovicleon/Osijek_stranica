52b9c4da05994c9e2678852c04451bae073a5512
kurtovicleon <kurtovicleon@gmail.com>
2018-04-04 14:41:31 +0200

    bazaful

--- bazaosijek.sql
+++ bazaosijek.sql
@@ -8,6 +8,7 @@ create table korisnik(
 sifra int not null primary key auto_increment,
 ime varchar(50) not null,
 adresa varchar(50) not null,
+mjesto varchar(50) not null,
 kontaktbroj int not null,
 email varchar(50) not null
 );
@@ -16,7 +17,7 @@ create table dogadaji(
 sifra int not null primary key auto_increment,
 dan int not null,
 mjesto int not null,
-vrijeme varchar(50),
+vrijeme DATETIME,
 cijena decimal(18,2),
 tekst text not null,
 kategorija varchar(50),
@@ -29,7 +30,8 @@ create table dan(
 );
 create table mjesto(
 sifra int not null primary key auto_increment,
-putokaz varchar(50)
+putokazx varchar(50);
+putokazy varchar(50)
 );
 create table slika(
 sifra int not null primary key auto_increment,
@@ -41,3 +43,21 @@ alter table dogadaji add foreign key (korisnik) references korisnik(sifra);
 alter table dogadaji add foreign key (dan) references dan(sifra);
 alter table dogadaji add foreign key (mjesto) references mjesto(sifra);
 alter table slika add foreign key (dogadaji) references dogadaji(sifra);
+
+insert into korisnik(ime,adresa,mjesto,kontaktbroj,email) VALUES
+('Caffe bar Osijek','Radićeva 10','Osijek',38595645987,'caffeos@osijek.hr'),
+('Restoran Konoba','Županijska 15','Osijek',38596487613,'konoba@osijek.hr'),
+('Muzej mursa','Europska avenija 16','Osijek'),38598493829,'mursaos@osijek.hr');
+
+insert into mjesto(putokazx,putokazy) VALUES
+(45.34294,34.4329),
+(45.432,34.432);
+
+insert into događaj(vrijeme,cijena,tekst,kategorija)
+values 
+('19:30:10',20,'Dođite na najbolji party godine','Zabava'),
+('17:20:00',55,'Dobar koncert za vas i vaše najdraže');
+
+insert into dan(datum)
+VALUES ('2018-03-28'),
+('2018-03-23');
